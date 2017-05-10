e08893d1d1891526bc088b65d07e69ba3082f53f
Ghiles Sidi Saïd <dev.for.world@gmail.com>
2017-05-10 15:09:52 +0100

    Add a splash screen

---
+++ app/src/main/java/com/esi/mxt07/pfe2017/alpr/SplashScreenActivity.java
@@ -0,0 +1,29 @@
+package com.esi.mxt07.pfe2017.alpr;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(1000);
+                    Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
+    }
+}
