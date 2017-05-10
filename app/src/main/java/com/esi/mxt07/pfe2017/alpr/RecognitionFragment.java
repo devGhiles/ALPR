d3c03e84d01d24148e430b7a71461049e5544247
Ghiles Sidi Saïd <dev.for.world@gmail.com>
2017-05-10 11:01:20 +0100

    Camera preview works, but the orientation is bad

--- app/src/main/java/com/esi/mxt07/pfe2017/alpr/RecognitionFragment.java
+++ app/src/main/java/com/esi/mxt07/pfe2017/alpr/RecognitionFragment.java
@@ -6,11 +6,21 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.hardware.Camera;
+import android.widget.Toast;
 
 
 public class RecognitionFragment extends Fragment implements View.OnClickListener {
+
+    private SurfaceView preview = null;
+    private SurfaceHolder previewHolder = null;
+    private Camera camera = null;
+    private boolean inPreview = false;
+    private boolean cameraConfigured = false;
 
     public RecognitionFragment() {
         // Required empty public constructor
@@ -20,18 +30,114 @@ public class RecognitionFragment extends Fragment implements View.OnClickListene
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        // inflate the layout
         View root = inflater.inflate(R.layout.fragment_recognition, container, false);
         FloatingActionButton fabTakePicture = (FloatingActionButton)
                 root.findViewById(R.id.fabTakePicture);
         fabTakePicture.setOnClickListener(this);
+
+        // camera preview
+        preview = (SurfaceView) root.findViewById(R.id.preview);
+        previewHolder = preview.getHolder();
+        previewHolder.addCallback(surfaceCallback);
+        previewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        // return the layout
         return root;
+    }
+
+    @Override
+    public void onPause() {
+        if (inPreview) {
+            camera.stopPreview();
+        }
+
+        camera.release();
+        camera = null;
+        inPreview = false;
+
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        camera = Camera.open();
+        startPreview();
     }
 
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.fabTakePicture) {
             DialogFragment plateDialogFragment = new PlateDialogFragment();
-            plateDialogFragment.show(getActivity().getSupportFragmentManager(), "RecognitionFragment");
+            plateDialogFragment.show(getActivity().getSupportFragmentManager(),
+                    "RecognitionFragment");
         }
     }
+
+    private Camera.Size getBestPreviewSize(int width, int height, Camera.Parameters parameters) {
+        Camera.Size result = null;
+
+        for (Camera.Size size : parameters.getSupportedPreviewSizes()) {
+            if (size.width <= width && size.height <= height) {
+                if (result == null) {
+                    result = size;
+                } else {
+                    int resultArea = result.width * result.height;
+                    int newArea = size.width * size.height;
+
+                    if (newArea > resultArea) {
+                        result = size;
+                    }
+                }
+            }
+        }
+
+        return (result);
+    }
+
+    private void initPreview(int width, int height) {
+        if (camera != null && previewHolder.getSurface() != null) {
+            try {
+                camera.setPreviewDisplay(previewHolder);
+            }
+            catch (Throwable t) {
+                Toast.makeText(getActivity(), t.getMessage(), Toast.LENGTH_LONG).show();
+            }
+
+            if (!cameraConfigured) {
+                Camera.Parameters parameters = camera.getParameters();
+                Camera.Size size=getBestPreviewSize(width, height, parameters);
+
+                if (size != null) {
+                    parameters.setPreviewSize(size.width, size.height);
+                    camera.setParameters(parameters);
+                    cameraConfigured=true;
+                }
+            }
+        }
+    }
+
+    private void startPreview() {
+        if (cameraConfigured && camera != null) {
+            camera.startPreview();
+            inPreview = true;
+        }
+    }
+
+    SurfaceHolder.Callback surfaceCallback=new SurfaceHolder.Callback() {
+        public void surfaceCreated(SurfaceHolder holder) {
+            // no-op -- wait until surfaceChanged()
+        }
+
+        public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            initPreview(width, height);
+            startPreview();
+        }
+
+        public void surfaceDestroyed(SurfaceHolder holder) {
+            // nothing
+        }
+    };
 }
